548b7ae564600c040ad8a111600a1baa91370184
ivanmdms <149631252+ivanmdms@users.noreply.github.com>
2026-02-11 19:14:16 -0300

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    // Garante que process.env.API_KEY esteja disponível no lado do cliente
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
+  }
+});
